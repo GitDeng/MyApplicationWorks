489848f3316018bf295787e45b1302597a3cc432
JackRobot <163djd@gmail.com>
2017-01-06 14:04:53 +0800

    调整为使用butterknife框架

--- MyApplicationWorks/app/src/main/java/cn/loverobots/myapplicationworks/activity/MainActivity.java
+++ MyApplicationWorks/app/src/main/java/cn/loverobots/myapplicationworks/activity/MainActivity.java
@@ -5,50 +5,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.loverobots.myapplicationworks.R;
 import cn.loverobots.myapplicationworks.utils.ToastUtil;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mBtnHotMovie, mBtnStockEagle, mBtnXyzReader, mBtnMostNews, mBtnGraduationProject;
+    @BindView(R.id.btn_hot_movie)
+    Button mBtnHotMovie;
+    @BindView(R.id.btn_stock_eagle)
+    Button mBtnStockEagle;
+    @BindView(R.id.btn_xyz_reader)
+    Button mBtnXyzReader;
+    @BindView(R.id.btn_most_news)
+    Button mBtnMostNews;
+    @BindView(R.id.btn_graduation_project)
+    Button mBtnGraduationProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         init();
     }
 
     private void init() {
-        mBtnHotMovie = (Button) findViewById(R.id.btn_hot_movie);
-        mBtnStockEagle = (Button) findViewById(R.id.btn_stock_eagle);
-        mBtnXyzReader = (Button) findViewById(R.id.btn_xyz_reader);
-        mBtnMostNews = (Button) findViewById(R.id.btn_most_news);
-        mBtnGraduationProject = (Button) findViewById(R.id.btn_graduation_project);
 
-        mBtnHotMovie.setOnClickListener(this);
-        mBtnStockEagle.setOnClickListener(this);
-        mBtnXyzReader.setOnClickListener(this);
-        mBtnMostNews.setOnClickListener(this);
-        mBtnGraduationProject.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_hot_movie: // 最热电影
+    @OnClick({R.id.btn_hot_movie, R.id.btn_stock_eagle, R.id.btn_xyz_reader, R.id.btn_most_news, R.id.btn_graduation_project})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_hot_movie:
                 ToastUtil.showToast(MainActivity.this, R.string.btn_hot_movie);
                 break;
-            case R.id.btn_stock_eagle: // 股票雄鹰
+            case R.id.btn_stock_eagle:
                 ToastUtil.showToast(MainActivity.this, R.string.btn_stock_eagle);
                 break;
-            case R.id.btn_xyz_reader: // xyz阅读器
+            case R.id.btn_xyz_reader:
                 ToastUtil.showToast(MainActivity.this, R.string.btn_xyz_reader);
                 break;
-            case R.id.btn_most_news: // 最新闻
+            case R.id.btn_most_news:
                 ToastUtil.showToast(MainActivity.this, R.string.btn_most_news);
                 break;
-            case R.id.btn_graduation_project: // 毕业设计
+            case R.id.btn_graduation_project:
                 ToastUtil.showToast(MainActivity.this, R.string.btn_graduation_project);
                 break;
         }
